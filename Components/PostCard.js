9b0c0999504d8f195e3d99141183d04408f3948d
unknown <tawsif.lucse@gmail.com>
2022-03-11 16:29:47 +0600

    post details-page loaded with comment

--- Components/PostCard.js
+++ Components/PostCard.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import Link from 'next/link';
 import styles from '../styles/PostCard.module.css'
+import { useRouter } from 'next/router';
 const PostCard = ({ post }) => {
+    const router = useRouter();
+    function handleShowPost() {
+        router.push('/' + post.id.toString());
+    }
+
     return (
         <div className={styles.post_card}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
-            <button className={styles.button}> <Link href={``}>Show Details</Link> </button>
+            <button className={styles.button} onClick={handleShowPost}>Show Details</button>
         </div>
     );
 };
+
+
 
 export default PostCard;
---
+++ pages/[post_details]/index.js
@@ -0,0 +1,54 @@
+function PostDetails({ post, filterComments }) {
+    console.log(filterComments);
+    return (
+        <div className="container">
+            <h1>{post?.title}- {post.id}</h1>
+            <h3>{post?.body}</h3>
+            <h3>Comments</h3>
+            {
+                filterComments.map(comment => (
+                    <div key={comment.id}>
+                        <h4 >{comment.name}</h4>
+                        <h6>{comment.email}</h6>
+                        <p>{comment.body}</p>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export async function getStaticPaths() {
+    // Call an external API endpoint to get posts
+    const postsRes = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await postsRes.json()
+    // Get the paths we want to pre-render based on posts
+    const paths = posts.slice(0, 9).map((post) => ({
+        params: {
+            post_details: post.id.toString()
+        },
+    }))
+
+
+
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+
+    return { paths, fallback: false }
+}
+
+
+export async function getStaticProps({ params }) {
+
+    const postRes = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.post_details}`);
+    const post = await postRes.json();
+
+    const commentRes = await fetch(`https://jsonplaceholder.typicode.com/comments`);
+    const comments = await commentRes.json();
+
+    const filterComments = comments.filter(comment => (comment.postId == params.post_details));
+
+    return { props: { post, filterComments } }
+}
+
+export default PostDetails;
