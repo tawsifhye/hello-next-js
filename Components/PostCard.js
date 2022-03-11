f97889ab220969f36b614a837cafc0dc5ba34eb6
unknown <tawsif.lucse@gmail.com>
2022-03-11 12:44:28 +0600

    Details page added

--- Components/PostCard.js
+++ Components/PostCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '../styles/PostCard.module.css'
 const PostCard = ({ post }) => {
     return (
         <div className={styles.post_card}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
+            <button className={styles.button}> <Link href={``}>Show Details</Link> </button>
         </div>
     );
 };
---
+++ pages/blogs/[post-details]/index.js
@@ -0,0 +1,10 @@
+
+
+function PostDetails() {
+
+    return (
+        <h1>Post Details</h1>
+    );
+}
+
+export default PostDetails;
